e29a7f7d577c057d7d47cb6f4f3f15af62a6afa7
aliciac <alicia.charef@hotmail.fr>
2025-02-27 15:29:37 +0100

    feat: correct for firebaseConfig and git ignore

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,30 +1,35 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// 
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
+
+
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8PuAxL0pCIC8FSbdi-L1V4cXqSHoym7g",
-  authDomain: "simplon-learn-b978a.firebaseapp.com",
-  projectId: "simplon-learn-b978a",
-  storageBucket: "simplon-learn-b978a.appspot.com",
-  messagingSenderId: "366490921813",
-  appId: "1:366490921813:web:51aa7c71e0f4eb239adddc"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-
 const storage = getStorage(app);
 
-export {auth, db, storage}
+export { auth, db, storage };
+
+// 
